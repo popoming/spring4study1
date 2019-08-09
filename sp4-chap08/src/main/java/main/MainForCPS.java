5fad22c8a067fb72a926a7a7bc60163f4a4a6822
starcrash <popoming89@gmail.com>
2019-08-09 09:25:52 +0900

    통합 커밋1

---
+++ sp4-chap08/src/main/java/main/MainForCPS.java
@@ -0,0 +1,28 @@
+package main;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import spring.ChangePasswordService;
+import spring.IdPasswordNotMatchingException;
+import spring.MemberNotFoundException;
+
+public class MainForCPS {
+
+	public static void main(String[] args) {
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext("classpath:appCtx.xml");
+		
+		ChangePasswordService cps = ctx.getBean("changePwSvc", ChangePasswordService.class);
+		
+		try {
+			cps.changePassword("anwjr89@naver.com", "1234", "1111");
+			System.out.println("암호를 변경했습니다.");
+		} catch (MemberNotFoundException e) {
+			System.out.println("회원 데이터가 존재하지 않습니다.");
+		} catch (IdPasswordNotMatchingException e) {
+			System.out.println("암호가 올바르지 않습니다.");
+		}
+		
+		ctx.close();
+	}
+}
---
+++ sp4-chap08/src/main/java/spring/MemberDao.java
@@ -0,0 +1,89 @@
+package spring;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+public class MemberDao {
+
+	private JdbcTemplate jdbcTemplate;
+
+	public MemberDao(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	public Member selectByEmail(String email) {
+		// TODO 구현
+		List<Member> results = jdbcTemplate.query("select * from spring4fs.MEMBER where EMAIL = ?", new RowMapper<Member>() {
+
+			@Override
+			public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Member member = new Member(rs.getString("EMAIL"), rs.getString("PASSWORD"), rs.getString("NAME"),
+						rs.getTimestamp("REGDATE"));
+				member.setId(rs.getLong("ID"));
+				// TODO Auto-generated method stub
+				return member;
+			}
+		}, email);
+
+		return results.isEmpty() ? null : results.get(0);
+	}
+
+	public void insert(final Member member) {
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				PreparedStatement pstmt = con.prepareStatement(
+						"insert into spring4fs.MEMBER (EMAIL, PASSWORD, NAME, REGDATE) "
+						+ "values(?, ?, ?, ?)"
+						+ new String[] {"ID"});
+				pstmt.setString(1, member.getEmail());
+				pstmt.setString(2, member.getPassword());
+				pstmt.setString(3, member.getName());
+				pstmt.setTimestamp(4, new Timestamp(member.getRegisterDate().getTime()));
+				return pstmt;
+			}
+		}, keyHolder);
+		Number keyValue = keyHolder.getKey();
+		member.setId(keyValue.longValue());
+	}
+
+	public void update(Member member) {
+		jdbcTemplate.batchUpdate("update spring4fs.MEMBER set PASSWORD = ? where EMAIL = ?",
+				member.getPassword(), member.getEmail());
+	}
+
+	public List<Member> selectAll() {
+		List<Member> results = jdbcTemplate.query("select * from spring4fs.MEMBER", new RowMapper<Member>() {
+
+			@Override
+			public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Member member = new Member(rs.getString("EMAIL"), rs.getString("PASSWORD"), rs.getString("NAME"),
+						rs.getTimestamp("REGDATE"));
+				member.setId(rs.getLong("ID"));
+				return member;
+			}
+
+		});
+
+		return results;
+	}
+
+	public int count() {
+		Integer count = jdbcTemplate.queryForObject("select count(*) form spring4fs.MEMBER", Integer.class);
+		return count;
+	}
+}
